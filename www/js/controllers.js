25530b2585ec7d2228e37a5e365f996d87ea83fe
BabyManisha <babymaneesha@gmail.com>
2016-11-19 13:33:54 +0530

    added username

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,30 +1,34 @@
 angular.module('deals.controllers', [])
-	// var baseURL = "http://localhost:8000/",
-	// 	userName = "";
 
     .controller('DealsLoginCtrl', function ($scope, $http, $location) {
     	$scope.baseURL = "http://localhost:8000/";
     	$scope.userName = "";
     	
     	$scope.FBLogin = function(){
-	        FB.login(function(response) {
-	            if (response.authResponse) {
-	             console.log('Welcome!  Fetching your information.... ');
-	             FB.api('/me', function(response) {
-	               console.log('Good to see you, ' + response.name + '.');
-	               
-	               var accessToken = FB.getAuthResponse();
-	               console.log(accessToken);
-	               if(accessToken.userID != null){
-	                    $scope.userName = accessToken.userID;
-	                    console.log($scope.userName);
-	               		$location.path("/userHome");
-	               }
-	             });
-	            } else {
-	             console.log('User cancelled login or did not fully authorize.');
-	            }
-	        });
+    		if(username){
+    			$location.path("/userHome");
+    		}else{
+    			FB.login(function(response) {
+    			    if (response.authResponse) {
+    			     console.log('Welcome!  Fetching your information.... ');
+    			     FB.api('/me', function(response) {
+    			       console.log('Good to see you, ' + response.name + '.');
+    			       username = response.name;
+    			       console.log(username);
+    			       var accessToken = FB.getAuthResponse();
+    			       console.log(accessToken);
+    			       if(accessToken.userID != null){
+    			            // $scope.userName = accessToken.userID;
+    			            userid = accessToken.userID;
+    			            console.log(username);
+    			       		$location.path("/userHome");
+    			       }
+    			     });
+    			    } else {
+    			     console.log('User cancelled login or did not fully authorize.');
+    			    }
+    			});
+    		}
 	    };
 
 
@@ -47,17 +51,15 @@ angular.module('deals.controllers', [])
 
     .controller('InvalidLoginCtrl', function ($scope) {})
 
-    .controller('DealsHomeCtrl', function ($scope) {})
+    .controller('DealsHomeCtrl', function ($scope) {
+    	$scope.username = username;
+    })
 
     .controller('AllDealsCtrl', function ($scope, $http) {
-  //   	$http.get("https://sheetsu.com/apis/v1.0/f30264ebda07").success(function(response){
-		// 	$scope.deals = response;
-		// });
-
-		http.get("https://sheetsu.com/apis/v1.0/f30264ebda07").then(function(response) {
-	        $scope.deals = response;
-	    }, function(response) {
-	        //Second function handles error
+    	$http.get("https://sheetsu.com/apis/v1.0/f30264ebda07").success(function(response){
+			$scope.deals = response;
+		})
+		.error(function(error){
 	        console.log("Some Error occured in Api");
-	    });
+		});
     });
